e6957e4dca44b3a93413be7530e3ad01e7322fda
BillyMRX1 <brilianadeputra@gmail.com>
2023-11-21 17:38:17 +0700

    Update data

--- data/books.js
+++ data/books.js
@@ -1,24 +1,3 @@
-const books = [
-  {
-    name: "Buku A",
-    year: 2010,
-    author: "John Doe",
-    summary: "Lorem ipsum dolor sit amet",
-    publisher: "Dicoding Indonesia",
-    pageCount: 100,
-    readPage: 25,
-    reading: true,
-  },
-  {
-    name: "dicoding",
-    year: 2011,
-    author: "John Doek",
-    summary: "Lorem ipsum dolor sit amet",
-    publisher: "Dicoding Indonesia",
-    pageCount: 100,
-    readPage: 25,
-    reading: false,
-  },
-];
+const books = [];
 
 export { books };
